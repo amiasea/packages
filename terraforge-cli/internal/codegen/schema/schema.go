008c08c489c591378367f1c80bd2c35b1d672be3
Alfredo Ball <alfredo.ball@outlook.com>
2026-01-26 15:14:04 -0600

    APIReferenceURL

--- terraforge-cli/internal/codegen/schema/schema.go
+++ terraforge-cli/internal/codegen/schema/schema.go
@@ -1,43 +1,44 @@
 package schema
 
 import (
-    "encoding/json"
-    "fmt"
-    "os"
+	"encoding/json"
+	"fmt"
+	"os"
 )
 
 type Schema struct {
-    Provider  ProviderDefinition  `json:"provider"`
-    Resources []ResourceDefinition `json:"resources"`
+	Provider  ProviderDefinition   `json:"provider"`
+	Resources []ResourceDefinition `json:"resources"`
 }
 
 type ProviderDefinition struct {
-    Name    string `json:"name"`
-    Version string `json:"version"`
+	Name            string `json:"name"`
+	Version         string `json:"version"`
+	APIReferenceURL string `json:"api_reference_url"`
 }
 
 type ResourceDefinition struct {
-    Name   string            `json:"name"`
-    Type   string            `json:"type"`
-    Fields []FieldDefinition `json:"fields"`
+	Name   string            `json:"name"`
+	Type   string            `json:"type"`
+	Fields []FieldDefinition `json:"fields"`
 }
 
 type FieldDefinition struct {
-    Name     string `json:"name"`
-    Type     string `json:"type"`
-    Required bool   `json:"required"`
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Required bool   `json:"required"`
 }
 
 func Load(path string) (*Schema, error) {
-    data, err := os.ReadFile(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read schema file: %w", err)
-    }
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read schema file: %w", err)
+	}
 
-    var s Schema
-    if err := json.Unmarshal(data, &s); err != nil {
-        return nil, fmt.Errorf("failed to parse schema: %w", err)
-    }
+	var s Schema
+	if err := json.Unmarshal(data, &s); err != nil {
+		return nil, fmt.Errorf("failed to parse schema: %w", err)
+	}
 
-    return &s, nil
+	return &s, nil
 }
